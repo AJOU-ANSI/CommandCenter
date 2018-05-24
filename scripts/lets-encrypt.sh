2473d1c49180924fa1e4a2b99833fc462b69e17f
Ubuntu <ubuntu@ip-172-31-31-122.ap-northeast-2.compute.internal>
2018-05-24 15:47:55 +0000

    Modify SUPER_ADMIN_TOKEN from hard coding to env file

---
+++ scripts/lets-encrypt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo docker run -it --rm --name certbot
+    -v "/etc/letsencrypt:/etc/letsencrypt"
+    -v "/var/lib/letsencrypt:/var/lib/letsencrypt"
+    -p 80:80
+    certbot/certbot certonly
